a367ca0b6b2391be2246f6eb27d67a77ef041b4d
Nik26sal <114744576+Nik26sal@users.noreply.github.com>
2024-03-23 18:55:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function f1() {
+    document.getElementById('a1').style.display = document.getElementById('a1').style.display === "block" ? "none" : "block";
+    document.getElementById('btn1').innerHTML=document.getElementById('btn1').innerHTML === "+"? "x":"+";
+}
+function f2(){
+    document.getElementById('a2').style.display = document.getElementById('a2').style.display === "block" ? "none" : "block";
+    document.getElementById('btn2').innerHTML=document.getElementById('btn2').innerHTML === "+"? "x":"+";
+}function f3(){
+    document.getElementById('a3').style.display = document.getElementById('a3').style.display === "block" ? "none" : "block";
+    document.getElementById('btn3').innerHTML=document.getElementById('btn3').innerHTML === "+"? "x":"+";
+}function f4(){
+    document.getElementById('a4').style.display = document.getElementById('a4').style.display === "block" ? "none" : "block";
+    document.getElementById('btn4').innerHTML=document.getElementById('btn4').innerHTML === "+"? "x":"+";
+}
+function f5(){
+    document.getElementById('a5').style.display = document.getElementById('a5').style.display === "block" ? "none" : "block";
+    document.getElementById('btn5').innerHTML=document.getElementById('btn5').innerHTML === "+"? "x":"+";
+}
+function f6(){
+    document.getElementById('a6').style.display = document.getElementById('a6').style.display === "block" ? "none" : "block";
+    document.getElementById('btn6').innerHTML=document.getElementById('btn6').innerHTML === "+"? "x":"+";
+}
